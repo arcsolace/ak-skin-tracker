c27a5e4b206ae449beee3a13f4b60e246b905097
Jay Kh <jayjaystownjo@gmail.com>
2023-07-15 00:44:30 +0700

    add utils

---
+++ utils/utils.go
@@ -0,0 +1,18 @@
+package utils
+
+import {
+	"math/rand"
+	"time"
+}
+
+func GenerateRandomUserCode(length int) string {
+    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+    userCode := make([]byte, length)
+    for i := 0; i < length; i++ {
+        userCode[i] = charset[seededRand.Intn(len(charset))]
+    }
+
+    return string(userCode)
+}
